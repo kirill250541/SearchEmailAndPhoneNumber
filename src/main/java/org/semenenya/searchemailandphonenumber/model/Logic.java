c0433a178f2d85ca6d68189aed95a3dc5a6c2389
kirill250541 <132009360+kirill250541@users.noreply.github.com>
2024-12-14 13:06:57 +0300

    Update Logic.java

--- src/main/java/org/semenenya/searchemailandphonenumber/model/Logic.java
+++ src/main/java/org/semenenya/searchemailandphonenumber/model/Logic.java
@@ -10,31 +10,35 @@ public class Logic {
     private Logic(){}
     public static String choiceRegularExpression(int flag){
         String regex1 = "([a-z0-9]{1,}[\\\\-]{0,1}[a-z0-9]{1,}[\\\\.]{0,1}[a-z0-9]{1,})" +
-                "+@([a-z0-9]{1,}[\\\\-]{0,1}[a-z0-9]{1,}[\\\\.]{0,1}[a-z0-9]{1,})+[\\\\.]{1}[a-z]{2,4}"
-                ;
+                "+@([a-z0-9]{1,}[\\\\-]{0,1}[a-z0-9]{1,}[\\\\.]{0,1}[a-z0-9]{1,})+[\\\\.]{1}[a-z]{2,4}";
         String regex2 = "([a-z]{1,}[\\\\-]{0,1}[a-z]{1,}[\\\\.]{0,1}[0-9]{7})";
 
-        if(flag==0)
+        if(flag==0){
             return regex1;
-
-        else if (flag==1)
+        }
+        else if (flag==1){
             return regex2;
-
-        else
+        }
+        else{
             return null;
+        }
     }
     public static List<String> searchEmailOrTelephoneNumber(String text, int flag){
         List<String> list = new ArrayList<>();
 
         String regularExpression= choiceRegularExpression(flag);
-        if(regularExpression==null)
+        
+        if(regularExpression==null){
             return Collections.emptyList();
-
+        }
+        
         Pattern pattern = Pattern.compile(regularExpression, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(text);
-        while (matcher.find())
+        
+        while (matcher.find()){
             list.add(matcher.group());
-
+        }
+        
         return list;
     }
 }
